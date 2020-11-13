98a552cc26652d7a1bffa32d065cc323aa7a827c
_uchanlee <uchanlee.dev@gmail.com>
2020-11-13 21:02:59 +0900

    [CREATE] #3 UserController

---
+++ src/main/java/com/frame/admin/domain/user/controller/UserController.java
@@ -0,0 +1,11 @@
+package com.frame.admin.domain.user.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+}
