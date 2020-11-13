fbc75f6e0de5b69046a4ea195fbaf78119d85b78
_uchanlee <uchanlee.dev@gmail.com>
2020-11-13 22:45:29 +0900

    [CREATE] #4 Admin Repository

---
+++ src/main/java/com/frame/admin/domain/admin/domain/repository/AdminRepository.java
@@ -0,0 +1,7 @@
+package com.frame.admin.domain.admin.domain.repository;
+
+import com.frame.admin.domain.admin.domain.entity.Admin;
+import org.springframework.data.repository.CrudRepository;
+
+public interface AdminRepository extends CrudRepository<Admin, String> {
+}
