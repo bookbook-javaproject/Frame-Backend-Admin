b1329c1f6adcd1aa4addfedec9484631e9456282
_uchanlee <uchanlee.dev@gmail.com>
2020-11-13 22:48:06 +0900

    [DELETE] #3 User Repository

--- src/main/java/com/frame/admin/domain/user/domain/repository/UserRepository.java
+++
@@ -1,6 +0,0 @@
-package com.frame.admin.domain.user.domain.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserRepository implements CrudRepository<> {
-}
