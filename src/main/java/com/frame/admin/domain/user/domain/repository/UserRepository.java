c756381352c4389999f30855ebc972fec0a22145
_uchanlee <uchanlee.dev@gmail.com>
2020-11-13 21:09:29 +0900

    [CREATE] #4 UserRepository

---
+++ src/main/java/com/frame/admin/domain/user/domain/repository/UserRepository.java
@@ -0,0 +1,6 @@
+package com.frame.admin.domain.user.domain.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserRepository implements CrudRepository<> {
+}
