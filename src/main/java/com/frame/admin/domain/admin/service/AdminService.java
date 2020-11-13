5239e66cd36e9946dd5ae47776398a03b621b77a
_uchanlee <uchanlee.dev@gmail.com>
2020-11-13 22:47:26 +0900

    [CREATE] #5 Admin Service

---
+++ src/main/java/com/frame/admin/domain/admin/service/AdminService.java
@@ -0,0 +1,8 @@
+package com.frame.admin.domain.admin.service;
+
+import com.frame.admin.domain.admin.dto.RegisterRequest;
+
+public interface AdminService {
+
+    public void register(RegisterRequest request);
+}
