6eae4fc557d226ed012c07c5184a6033708dbc6a
_uchanlee <uchanlee.dev@gmail.com>
2020-11-13 22:50:01 +0900

    [CREATE] #10 WebSecurityConfig

---
+++ src/main/java/com/frame/admin/infra/springBoot/security/WebSecurityConfig.java
@@ -0,0 +1,25 @@
+package com.frame.admin.infra.springBoot.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .sessionManagement().disable()
+                .formLogin().disable()
+                .authorizeRequests()
+                .antMatchers(HttpMethod.POST, "/admin").permitAll()
+                .anyRequest().authenticated();
+    }
+}
