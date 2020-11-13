b2484180ce6df9036a8d3cc54e1f50d3ac114b71
_uchanlee <uchanlee.dev@gmail.com>
2020-11-13 23:26:41 +0900

    [ADD] #5 Admin login Service Implement

--- src/main/java/com/frame/admin/domain/admin/service/AdminServiceImpl.java
+++ src/main/java/com/frame/admin/domain/admin/service/AdminServiceImpl.java
@@ -1,7 +1,11 @@
 package com.frame.admin.domain.admin.service;
 
+import com.frame.admin.domain.admin.domain.usecase.CertifyUserUseCase;
 import com.frame.admin.domain.admin.domain.usecase.CreateAdminUseCase;
+import com.frame.admin.domain.admin.dto.LoginRequest;
+import com.frame.admin.domain.admin.dto.LoginResponse;
 import com.frame.admin.domain.admin.dto.RegisterRequest;
+import com.frame.admin.infra.springBoot.security.JwtProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -10,9 +14,21 @@ import org.springframework.stereotype.Component;
 public class AdminServiceImpl implements AdminService {
 
     private final CreateAdminUseCase createAdminUseCase;
+    private final CertifyUserUseCase certifyUserUseCase;
+
+    private final JwtProvider jwtProvider;
 
     @Override
     public void register(RegisterRequest request) {
         createAdminUseCase.execute(request);
+    }
+
+    @Override
+    public LoginResponse login(LoginRequest request) {
+        certifyUserUseCase.execute(request.getId(), request.getPassword());
+        return LoginResponse.builder()
+                .accessToken(jwtProvider.generateAccessToken(request.getId()))
+                .refreshToken(jwtProvider.generateRefreshToken(request.getId()))
+                .build();
     }
 }
