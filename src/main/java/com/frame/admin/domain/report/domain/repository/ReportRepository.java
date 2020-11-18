cdaee56dfbe4926b58e062dec4c335be1ced4208
_uchanlee <uchanlee.dev@gmail.com>
2020-11-18 13:50:46 +0900

    [CREATE] #16 ReportRepository

---
+++ src/main/java/com/frame/admin/domain/report/domain/repository/ReportRepository.java
@@ -0,0 +1,9 @@
+package com.frame.admin.domain.report.domain.repository;
+
+import com.frame.admin.domain.report.domain.entity.Report.Report;
+import org.springframework.data.repository.CrudRepository;
+
+
+public interface ReportRepository extends CrudRepository<Report, Long> {
+    public Iterable<Report> findAllByPostNumber(Long postId);
+}
