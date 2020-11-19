028a3584d756890703059b113405421e12bd3590
_uchanlee <uchanlee.dev@gmail.com>
2020-11-19 09:49:21 +0900

    [ADD] #16 Get Count About Reports By Post UseCase

--- src/main/java/com/frame/admin/domain/report/domain/usecase/GetCountAboutReportsByPostUseCase.java
+++ src/main/java/com/frame/admin/domain/report/domain/usecase/GetCountAboutReportsByPostUseCase.java
@@ -2,6 +2,6 @@ package com.frame.admin.domain.report.domain.usecase;
 
 import com.frame.admin.domain.report.domain.entity.Report.Report;
 
-public interface GetReportsByPostUseCase {
-    public Iterable<Report> execute(Long postId);
+public interface GetCountAboutReportsByPostUseCase {
+    public int execute(Long postId);
 }
