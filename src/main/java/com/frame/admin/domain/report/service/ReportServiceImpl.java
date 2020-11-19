b22b955d4c03f852c8fb8ac4d03b960d0dd34ffb
_uchanlee <uchanlee.dev@gmail.com>
2020-11-19 09:51:04 +0900

    [ADD] #16 Report Service Implement

--- src/main/java/com/frame/admin/domain/report/service/ReportServiceImpl.java
+++ src/main/java/com/frame/admin/domain/report/service/ReportServiceImpl.java
@@ -1,40 +1,36 @@
 package com.frame.admin.domain.report.service;
 import com.frame.admin.domain.post.domain.entity.Post.Post;
 import com.frame.admin.domain.post.domain.usecase.GetPostUseCase;
-import com.frame.admin.domain.report.domain.dto.GetReportsResponse;
+import com.frame.admin.domain.report.domain.dto.ReportVO;
 import com.frame.admin.domain.report.domain.entity.Report.Report;
-import com.frame.admin.domain.report.domain.usecase.GetReportsByPostUseCase;
+import com.frame.admin.domain.report.domain.usecase.GetCountAboutReportsByPostUseCase;
 import com.frame.admin.domain.report.domain.usecase.GetReportsUseCase;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class ReportServiceImpl implements ReportService {
     private final GetReportsUseCase getReportsUseCase;
-    private final GetReportsByPostUseCase getReportsByPostUseCase;
+    private final GetCountAboutReportsByPostUseCase getCountAboutReportsByPostUseCase;
 
     private final GetPostUseCase getPostUseCase;
 
     @Override
-    public List<GetReportsResponse> getReports() {
-        List<GetReportsResponse> reports = new ArrayList<GetReportsResponse>();
+    public List<ReportVO> getReports() {
+        List<ReportVO> reports = new ArrayList<ReportVO>();
         for (Report report : getReportsUseCase.execute()) {
             Long postId = report.getPostNumber();
-            int count = 0;
-            for (Report reportByPost : getReportsByPostUseCase.execute(postId)) {
-                count++;
-            }
-
             Post post = getPostUseCase.execute(postId);
             reports.add(
-                    GetReportsResponse.builder()
+                    ReportVO.builder()
                     .reportId(report.getReportNumber())
                     .content(report.getContent())
-                    .reportCount(Long.valueOf(count))
+                    .reportCount(Long.valueOf(getCountAboutReportsByPostUseCase.execute(postId)))
                     .writer(post.getWriter())
                     .reporter(report.getReporter())
                     .build()
