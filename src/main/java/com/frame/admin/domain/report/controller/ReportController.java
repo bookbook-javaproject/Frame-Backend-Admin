ed6a70cdb8e8bad519e75192e37036f4356038ad
_uchanlee <uchanlee.dev@gmail.com>
2020-11-19 09:47:57 +0900

    [UPDATE] #16 Get Reports Controller Request Object

--- src/main/java/com/frame/admin/domain/report/controller/ReportController.java
+++ src/main/java/com/frame/admin/domain/report/controller/ReportController.java
@@ -1,6 +1,6 @@
 package com.frame.admin.domain.report.controller;
 
-import com.frame.admin.domain.report.domain.dto.GetReportsResponse;
+import com.frame.admin.domain.report.domain.dto.ReportVO;
 import com.frame.admin.domain.report.service.ReportService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -14,7 +14,7 @@ public class ReportController {
     private final ReportService reportService;
 
     @GetMapping
-    public List<GetReportsResponse> getReports() {
+    public List<ReportVO> getReports() {
         return reportService.getReports();
     }
 }
