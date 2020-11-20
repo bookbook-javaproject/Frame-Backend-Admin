3e47ae492969e073ca9ca3769558a0a91f10ae5d
Maly <migsking@naver.com>
2020-11-20 23:57:01 +0900

    [CREATE] #23 Notice Service

--- src/main/java/com/frame/admin/domain/notice/service/NoticeService.java
+++ src/main/java/com/frame/admin/domain/notice/service/NoticeService.java
@@ -1,7 +1,10 @@
 package com.frame.admin.domain.notice.service;
 
 import com.frame.admin.domain.notice.dto.CreateNoticeRequest;
+import com.frame.admin.domain.notice.dto.GetNoticeResponse;
 
 public interface NoticeService {
+    public GetNoticeResponse getNotice();
+
     public void createNotice(CreateNoticeRequest request);
 }
