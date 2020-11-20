99bec1c3a0e6c5d31925d3567831822f755ec421
Maly <migsking@naver.com>
2020-11-20 23:36:22 +0900

    [CREATE] #23 Notice Controller

---
+++ src/main/java/com/frame/admin/domain/notice/controller/NoticeController.java
@@ -0,0 +1,31 @@
+package com.frame.admin.domain.notice.controller;
+
+import com.frame.admin.domain.admin.dto.LoginRequest;
+import com.frame.admin.domain.admin.dto.LoginResponse;
+import com.frame.admin.domain.admin.dto.RegisterRequest;
+import com.frame.admin.domain.notice.dto.CreateNoticeRequest;
+import com.frame.admin.domain.notice.service.NoticeService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/notice")
+@RequiredArgsConstructor
+public class NoticeController {
+    private final NoticeService noticeService;
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public void getNotice() {
+        // TODO
+    }
+
+    @PostMapping()
+    @ResponseStatus(HttpStatus.CREATED)
+    public void createNotice(@RequestBody @Valid CreateNoticeRequest request) {
+        noticeService.createNotice(request);
+    }
+}
