daa3f8fa7129f4adf1c279c616ca0c9b13445c7a
Maly <migsking@naver.com>
2020-11-21 00:38:18 +0900

    .

---
+++ src/main/java/com/frame/admin/domain/submission/domain/entity/AuthorSubmission.java
@@ -0,0 +1,28 @@
+package com.frame.admin.domain.submission.domain.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Getter
+@Builder
+@Entity
+@Table(name = "tbl_author_submission")
+@NoArgsConstructor
+@AllArgsConstructor
+public class AuthorSubmission {
+    @Id
+    private String email;
+
+    @Column(name = "link")
+    private String link;
+
+    @Column(name = "goal")
+    private String goal;
+}
