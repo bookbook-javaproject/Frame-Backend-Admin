600afeaae2b2c2e94ca1a9a2348a883d4ace81c7
_uchanlee <uchanlee.dev@gmail.com>
2020-11-22 21:12:53 +0900

    [ADD] #33 Approve Report Service Implement

--- src/main/java/com/frame/admin/domain/report/service/ReportServiceImpl.java
+++ src/main/java/com/frame/admin/domain/report/service/ReportServiceImpl.java
@@ -1,15 +1,18 @@
 package com.frame.admin.domain.report.service;
 import com.frame.admin.domain.post.domain.entity.Post.Post;
+import com.frame.admin.domain.post.domain.usecase.DeletePostUseCase;
 import com.frame.admin.domain.post.domain.usecase.GetPostUseCase;
+import com.frame.admin.domain.report.domain.dto.ApproveReportRequest;
 import com.frame.admin.domain.report.domain.dto.ReportVO;
 import com.frame.admin.domain.report.domain.entity.Report.Report;
+import com.frame.admin.domain.report.domain.usecase.DeleteReportRelatedPostUseCase;
 import com.frame.admin.domain.report.domain.usecase.GetCountAboutReportsByPostUseCase;
+import com.frame.admin.domain.report.domain.usecase.GetReportedPostIdUseCase;
 import com.frame.admin.domain.report.domain.usecase.GetReportsUseCase;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
 
 @Service
@@ -17,8 +20,12 @@ import java.util.List;
 public class ReportServiceImpl implements ReportService {
     private final GetReportsUseCase getReportsUseCase;
     private final GetCountAboutReportsByPostUseCase getCountAboutReportsByPostUseCase;
+    private final GetReportedPostIdUseCase getReportedPostIdUseCase;
+    private final DeleteReportRelatedPostUseCase deleteReportRelatedPostUseCase;
+
 
     private final GetPostUseCase getPostUseCase;
+    private final DeletePostUseCase deletePostUseCase;
 
     @Override
     public List<ReportVO> getReports() {
@@ -37,5 +44,11 @@ public class ReportServiceImpl implements ReportService {
             );
         }
         return reports;
+    }
+
+    @Override
+    public void approveReport(ApproveReportRequest request) {
+        deletePostUseCase.execute(getReportedPostIdUseCase.execute(request.getReportId()));
+        deleteReportRelatedPostUseCase.execute(request.getReportId());
     }
 }
