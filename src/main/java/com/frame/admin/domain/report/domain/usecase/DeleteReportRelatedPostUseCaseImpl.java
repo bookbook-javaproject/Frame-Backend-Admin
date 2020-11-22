6dff654616ceb9618e7fcfc030edc524b023fbb5
_uchanlee <uchanlee.dev@gmail.com>
2020-11-22 21:13:12 +0900

    [ADD] #33 Delete Report Related Post UseCase Implement

---
+++ src/main/java/com/frame/admin/domain/report/domain/usecase/DeleteReportRelatedPostUseCaseImpl.java
@@ -0,0 +1,25 @@
+package com.frame.admin.domain.report.domain.usecase;
+
+import com.frame.admin.domain.report.domain.entity.Report.Report;
+import com.frame.admin.domain.report.domain.repository.ReportRepository;
+import com.frame.admin.global.exceptions.NotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component
+@RequiredArgsConstructor
+public class DeleteReportRelatedPostUseCaseImpl implements DeleteReportRelatedPostUseCase {
+
+    private ReportRepository reportRepository;
+
+    @Override
+    public void execute(Long reportId) {
+        Optional<Report> optionalReport = reportRepository.findById(reportId);
+        optionalReport.orElseThrow(NotFoundException::new);
+        optionalReport.ifPresent(report -> {
+            reportRepository.deleteAllByPostNumber(report.getPostNumber());
+        });
+    }
+}
