faa1fd88b282efaebca8009a8c2815ffb905eaa9
_uchanlee <uchanlee.dev@gmail.com>
2020-11-22 21:12:40 +0900

    [ADD] #33 Approve Report Service

--- src/main/java/com/frame/admin/domain/report/service/ReportService.java
+++ src/main/java/com/frame/admin/domain/report/service/ReportService.java
@@ -1,9 +1,12 @@
 package com.frame.admin.domain.report.service;
 
+import com.frame.admin.domain.report.domain.dto.ApproveReportRequest;
 import com.frame.admin.domain.report.domain.dto.ReportVO;
 
 import java.util.List;
 
 public interface ReportService {
     public List<ReportVO> getReports();
+
+    void approveReport(ApproveReportRequest request);
 }
