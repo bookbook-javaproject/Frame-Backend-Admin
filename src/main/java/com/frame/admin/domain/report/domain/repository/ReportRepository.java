10d3afa41aae8a15367e2356eded71be56abca9a
_uchanlee <uchanlee.dev@gmail.com>
2020-11-22 22:54:44 +0900

    [ADD] #33 Report Repository Transactional

--- src/main/java/com/frame/admin/domain/report/domain/repository/ReportRepository.java
+++ src/main/java/com/frame/admin/domain/report/domain/repository/ReportRepository.java
@@ -2,10 +2,12 @@ package com.frame.admin.domain.report.domain.repository;
 
 import com.frame.admin.domain.report.domain.entity.Report.Report;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 
 public interface ReportRepository extends CrudRepository<Report, Long> {
     public int countByPostNumber(Long postId);
 
+    @Transactional
     public void deleteAllByPostNumber(Long postId);
 }
